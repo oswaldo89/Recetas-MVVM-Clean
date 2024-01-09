38edbea0e668dec3a756c62419f1a69130f1c33c
oswaldogh89 <oswaldo.gh89@gmail.com>
2024-01-09 00:11:12 -0600

    initial commit

---
+++ app/src/main/java/com/recetasyape/app/WelcomeActivity.kt
@@ -0,0 +1,37 @@
+package com.recetasyape.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.recetasyape.app.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSplashDuration()
+
+        binding.apply {
+            startButton.setOnClickListener {
+                startActivity(Intent(this@WelcomeActivity, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+    private fun setSplashDuration() {
+        var splashScreenStays = true
+        val delayTime = 1000L
+
+        installSplashScreen().setKeepOnScreenCondition { splashScreenStays }
+        Handler(Looper.getMainLooper()).postDelayed({ splashScreenStays = false }, delayTime)
+    }
+}
