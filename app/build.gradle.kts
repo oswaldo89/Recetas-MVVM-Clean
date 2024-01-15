dc008f16d09277e935e37efdc82b2cf47ee035d5
oswaldogh89 <oswaldo.gh89@gmail.com>
2024-01-14 22:26:21 -0600

    se agrupa dependencias por tipo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,29 +47,45 @@ android {
 
 dependencies {
 
+    // Android
     implementation(Deps.coreKtx)
     implementation(Deps.appCompat)
     implementation(Deps.material)
     implementation(Deps.constraintLayout)
     implementation(Deps.playServicesMaps)
+
+    // Testing
     testImplementation(Deps.junit)
     androidTestImplementation(Deps.junitExt)
     androidTestImplementation(Deps.espressoCore)
+
+    // UI Components
     implementation(Deps.floatingSearchView)
     implementation(Deps.coreSplashScreen)
     implementation(Deps.navigationFragmentKtx)
     implementation(Deps.navigationUiKtx)
+
+    // Networking
     implementation(Deps.retrofit)
     implementation(Deps.retrofitGson)
     implementation(Deps.okHttpLoggingInterceptor)
+
+    // Dependency Injection
     implementation(Deps.daggerHilt)
     kapt(Deps.daggerHiltCompiler)
+
+    // Lifecycle
     implementation(Deps.lifecycleLiveDataKtx)
     implementation(Deps.lifecycleViewModelKtx)
+
+    // AndroidX
     implementation(Deps.activityKtx)
     implementation(Deps.fragmentKtx)
+
+    // Image
     implementation(Deps.glide)
     kapt(Deps.glideCompiler)
+
 }
 
 tasks.register("validateLayoutDimensions") {
