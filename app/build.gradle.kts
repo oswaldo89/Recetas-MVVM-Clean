82bbf20848fd8c5d593bf318954e1ca534ff3d3c
oswaldogh89 <oswaldo.gh89@gmail.com>
2024-01-14 23:11:14 -0600

    configurando pruebas unitarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,17 @@ dependencies {
     implementation(Deps.glide)
     kapt(Deps.glideCompiler)
 
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.1")
+
 }
 
 tasks.register("validateLayoutDimensions") {
